a49a2fb3680ea4ece1d17ffbd00e8adfa1dce7ce
Marcel R <github.riga@icloud.com>
2021-01-21 11:04:55 +0100

    Add back simple file caching.

--- helpers.py
+++ helpers.py
@@ -6,12 +6,23 @@ Helpers and constants.
 
 import os
 import shutil
+import subprocess
 
+import six
 import numpy as np
 
 
-# data base directory on EOS
+# the project directory
+this_dir = os.path.dirname(os.path.abspath(__file__))
+
+# the local data directory for caching
+data_dir = os.path.join(this_dir, "data")
+
+# data base directory on CERN EOS
 eos_data_dir = "/eos/user/m/mrieger/swan_aws_data"
+
+# base url of files on CERN EOS
+eos_data_url = "https://cernbox.cern.ch/index.php/s/GAkUpZjhEbzi2Uy/download?path={}&files={}"
 
 # data constants
 n_constituents = 200
@@ -36,6 +47,56 @@ def _check_dataset_file(kind, index):
     return "{}_{}.npz".format(kind, index)
 
 
+def _wget(url, path):
+    # create the parent directory, remove the file if existing
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+    elif os.path.exists(path):
+        os.remove(path)
+
+    # build the wget command and run it
+    cmd = ["wget", "-O", path, url]
+    try:
+        subprocess.check_call(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except Exception as e:
+        raise Exception("download of url '{}' failed: {}".format(url, e))
+
+    return path
+
+
+def provide_file(kind, index):
+    """
+    Returns the absolute file path of a file of an input dataset given by *kind* ("train", "valid"
+    or "test") and its *index*. When the file is not locally accessible, it is downloaded first from
+    the public CERNBox directory.
+    """
+    # get the name of the file
+    file_name = _check_dataset_file(kind, index)
+
+    # make sure the user data directory exists
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    # when it's already in the user data directy, return the full path
+    file_path = os.path.join(data_dir, file_name)
+    if os.path.exists(file_path):
+        return file_path
+
+    # when the public eos data dir is accessible, copy the file in the the user data dir
+    has_eos = os.access(eos_data_dir, os.R_OK)
+    if has_eos:
+        public_path = os.path.join(eos_data_dir, file_name)
+        shutil.copy2(public_path, file_path)
+        return file_path
+
+    # otherwise, download it using the public CERNBox url
+    quote = six.moves.urllib.parse.quote
+    url = eos_data_url.format(quote("/", safe=""), quote(file_name, safe=""))
+    _wget(url, file_path)
+
+    return file_path
+
+
 def load_data(kind, start_file=0, stop_file=-1):
     """
     Loads a certain *kind* of dataset ("train", "valid", or "test") and returns the four-vectors of
@@ -48,12 +109,8 @@ def load_data(kind, start_file=0, stop_file=-1):
     if stop_file < 0:
         stop_file = n_files[kind] - 1
 
-    # get all local file paths
-    file_paths = []
-    for i in range(start_file, stop_file):
-        file_name = _check_dataset_file(kind, i)
-        file_path = os.path.join(eos_data_dir, file_name)
-        file_paths.append(file_path)
+    # get all local file paths via download or public access
+    file_paths = [provide_file(kind, i) for i in range(start_file, stop_file)]
 
     # instead of loading all files, storing their contents and concatenating in the end, which can
     # have a peak memory consumption of twice the inputs, define output arrays with the correct
